9b64c819d3a0ea23ace3a2783ff9363f5f473cf2
omrgrcn <omergercin@gmail.com>
2019-07-06 03:19:41 +0300

    database connection

---
+++ helper/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    mongoose.connect('mongodb://api_user:qqwwee123@ds347707.mlab.com:47707/markakod-interview-api', {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+    });
+
+    mongoose.connection.on('open', () => {
+        console.log('MongoDB: Connected');
+    });
+
+    mongoose.connection.on('error', (err) => {
+        console.log('MongoDB: Error', err);
+    });
+};
